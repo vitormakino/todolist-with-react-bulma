0a7056ae5d40969f42f623a86e9d888c14840718
Vitor Makino <vitor.makino@gmail.com>
2018-02-03 17:17:47 -0200

    Add firebase

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -11,11 +11,22 @@ export default class TodoForm extends React.Component {
         }
     }
     
+    handleKeyPress(event) {
+        if(event.key == 'Enter' && event.target.value) {
+            this.props.addTodo(event.target.value);
+            event.target.focus();
+            event.target.value = "";
+        }
+    }
+
     render() {
         return (
           <Panel.Block> 
             <Control>
-              <Input placeholder="What needs to be done?" onBlur={this.handleBlur.bind(this)} />
+              <Input placeholder="What needs to be done?"
+                     onBlur={this.handleBlur.bind(this)}
+                     onKeyPress={this.handleKeyPress.bind(this)}
+                     />
             </Control>
           </Panel.Block>  
         );
